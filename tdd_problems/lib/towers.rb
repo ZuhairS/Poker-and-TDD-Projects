fd8028019cfa0e258c32b962063cb2d468f90222
Zuhair A. Shaikh <Zuhair.A.Shaikh@outlook.com>
2017-06-07 14:47:02 -0700

    Create a spec passing towers of hanoi game

--- tdd_problems/lib/towers.rb
+++ tdd_problems/lib/towers.rb
@@ -1,10 +1,29 @@
 class Tower
+  attr_accessor :towers
   def initialize
     @towers = Array.new(3) {Array.new}
-    @towers.first = [1,2,3]
+    @towers[0] = [3,2,1]
   end
 
   def play
-
+    until won?
+      start_tower, end_tower = get_input
+      move(start_tower, end_tower)
+    end
   end
+
+  def move(start_tower, end_tower)
+    raise 'Starting tower is empty!' if towers[start_tower].empty?
+    if !towers[end_tower].empty? && towers[start_tower].last > towers[end_tower].last
+      raise 'Bigger disk can not be placed on a smaller disk!'
+    end
+    towers[end_tower] << towers[start_tower].pop
+  end
+
+  def won?
+    return true if towers[1].length == 3 || towers[2].length == 3
+    false
+  end
+
+
 end
--- tdd_problems/spec/towers_spec.rb
+++ tdd_problems/spec/towers_spec.rb
@@ -3,19 +3,53 @@ require 'towers'
 
 describe Tower do
   describe '#initialize' do
-    subject(:towers) { Tower.new }
+    subject(:towers) { Tower.new.towers }
     it 'should create 3 arrays' do
       expect(towers.length).to eq(3)
     end
 
+    it 'should initialize an array with 3 sorted numbers as the first array' do
+      expect(towers.first).to eq([3,2,1])
+    end
   end
 
   describe '#move' do
+    subject(:towers) { Tower.new }
+    it 'should pop a disc from one tower' do
+      towers.move(0, 1)
+      expect(towers.towers.first).to eq([3,2])
+    end
+
+    it 'should push a disc into another tower' do
+      towers.move(0, 1)
+      expect(towers.towers[1]).to eq([1])
+    end
+
+    it 'should raise error if tower being moved from is empty' do
+      expect { towers.move(2, 1) }.to raise_error('Starting tower is empty!')
+    end
+
+    it 'should raise error if disk being moved is bigger than top disk on end tower' do
+      towers.move(0, 1)
+
+      expect { towers.move(0, 1) }.to raise_error('Bigger disk can not be placed on a smaller disk!')
+    end
 
   end
 
   describe '#won?' do
+    subject(:towers) { Tower.new }
 
+    it 'should not allow win at the start' do
+      expect(towers).to_not be_won
+    end
+
+
+
+    it 'should end game if second or third tower are completely filled' do
+      towers.towers[0] = []
+      towers.towers[1] = [3,2,1]
+      expect(towers).to be_won
+    end
   end
-
 end
